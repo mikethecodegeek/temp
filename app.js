6ea38ca7d7a446dd28a5545f0cc8beb7bd7c5b29
Michael Sanford <mikethecodegeek@gmail.com>
2016-07-31 08:41:14 -0700

    ok

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 //const PORT = process.env.PORT || 3000;
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8081;
 
 var express = require('express');
 var morgan = require('morgan');
@@ -34,5 +34,5 @@ app.use('/', require('./routes/index'));
 
 var server = http.createServer(app);
 server.listen(port, ipaddress, err => {
-    console.log(err || `Server listening on port ${PORT}`);
+    console.log(err || `Server listening on port ${port}`);
 });
