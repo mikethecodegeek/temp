932de00e36c9c963461008a5fafd686a82da58ed
Michael Sanford <mikethecodegeek@gmail.com>
2016-07-31 08:10:16 -0700

    change

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /**
  * Created by Michael Sanford on 7-25-16
  */
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var express = require('express');
 var morgan = require('morgan');
@@ -31,6 +33,6 @@ app.use('/', require('./routes/index'));
 
 
 var server = http.createServer(app);
-server.listen(PORT, err => {
+server.listen(port, ipaddress, err => {
     console.log(err || `Server listening on port ${PORT}`);
 });
